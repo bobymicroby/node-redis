dbad36a9f7fa40386c673e0194341bfe277d708d
borislav ivanov <borislav.ivanov@redis.com>
2026-02-27 16:50:39 +0200

    Add TestableQueue interface and implement addCommand method

--- packages/client/lib/binary-headers/test-utils.ts
+++ packages/client/lib/binary-headers/test-utils.ts
@@ -3,7 +3,7 @@ import { ResponseHeaderEncoder } from './generated/response-header-codec';
 import { RequestHeaderDecoder } from './generated/request-header-codec';
 import { Decoder } from '../RESP/decoder';
 import type { RespVersions } from '../RESP/types';
-import RedisCommandsQueue, { type SocketChunk, type CommandArguments } from '../client/commands-queue';
+import RedisCommandsQueue, { type SocketChunk, type CommandArguments, type CommandOptions } from '../client/commands-queue';
 import MasterQueue from './master-queue';
 import { BinaryHeadersInterceptor } from './codec';
 import { STATIC_RESOLVER, NOOP_RESOLVER } from './eligibility';
@@ -394,7 +394,7 @@ export function largeBuffer(size: number, fill = 0x78): Buffer {
  * This allows tests to work with the queue implementation.
  */
 export interface TestableQueue {
-  addCommand<T = unknown>(args: CommandArguments): Promise<T>;
+  addCommand<T = unknown>(args: CommandArguments, options?: CommandOptions): Promise<T>;
   commandsToWrite(): Generator<SocketChunk>;
   processIncomingData(chunk: Buffer): void;
   readonly decoder: Decoder;
@@ -422,7 +422,7 @@ class MasterQueueAdapter implements TestableQueue {
     this.#queue = queue;
   }
 
-  addCommand<T = unknown>(args: CommandArguments): Promise<T> {
+  addCommand<T = unknown>(args: CommandArguments, _options?: CommandOptions): Promise<T> {
     return this.#queue.addCommand(args);
   }
 
